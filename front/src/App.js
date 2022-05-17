d1ad12d374a51c95b2cd2f939f9cb52091cc8201
Kotik322 <kotan23111995@gmail.com>
2022-05-17 13:35:01 +0300

    added react bootstrap, add Navbar to Header Compn

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
+import { Container } from "react-bootstrap" 
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to shop</h1>
 
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 }
